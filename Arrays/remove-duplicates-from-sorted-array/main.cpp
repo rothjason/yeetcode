4112814fd05629753d449f95076ce55426751a4f
Jason Roth <13rothjason@gmail.com>
2020-02-29 11:27:19 -0800

    Added main.cpp skeleton
    Added main and removeDuplicates prototype and definition

---
+++ Arrays/remove-duplicates-from-sorted-array/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int removeDuplicates(vector<int> &v);
+
+int main(){
+  vector <int> nums;
+
+
+
+}
+
+int removeDuplicates(vector<int> &v){
+  
+}
