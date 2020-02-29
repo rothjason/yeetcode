313ad22f62f86386ae85ebe7fe1a32f4a637087a
Jason Roth <13rothjason@gmail.com>
2020-02-29 11:49:46 -0800

    Implemented Delete Algorithm
    Trasnlated pseudo code of delete algorithm into removeDuplicates function. Also initalized test vector

--- Arrays/remove-duplicates-from-sorted-array/main.cpp
+++ Arrays/remove-duplicates-from-sorted-array/main.cpp
@@ -7,11 +7,38 @@ int removeDuplicates(vector<int> &v);
 
 int main(){
   vector <int> nums;
+  nums.push_back(1);
+  nums.push_back(1);
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(3);
+  nums.push_back(3);
+  nums.push_back(4);
+  nums.push_back(4);
+  nums.push_back(4);
+  nums.push_back(5);
+  nums.push_back(5);
+  nums.push_back(6);
+  nums.push_back(6);
+  nums.push_back(6);
+
 
 
 
 }
 
 int removeDuplicates(vector<int> &v){
-  
+  int length = 1;
+  for(int i = 1; v.size()!=length; i++){
+    if(v[i] == v[i-1]){
+      vector<int>::iterator ptr = v.begin();
+      advance(ptr,i);
+      v.erase(ptr);
+      i--;
+    }
+    else{
+      length++;
+    }
+  }
+  return length;
 }
