97f2d0b4b493facf0eb48507b45a09c880ba345f
Jason Roth <13rothjason@gmail.com>
2020-03-02 21:22:37 -0800

    Added reverseString algorithm
    Added reverseString function defintion

--- Strings/reverse_string.cpp
+++ Strings/reverse_string.cpp
@@ -7,11 +7,42 @@ void reverseString(vector<char> &s);
 
 int main(){
 vector <char> word;
+  word.push_back('J');
+  word.push_back('a');
+  word.push_back('s');
+  word.push_back('o');
+  word.push_back('n');
 
+
+  for(int i = 0; i < word.size(); i++){
+    cout << word[i];
+  }
+
+
+  cout << endl;
+
+  reverseString(word);
+
+
+  for(int i = 0; i < word.size(); i++){
+    cout << word[i];
+  }
+
+  cout << endl;
 
 
 }
 
 void reverseString(vector<char> &s){
+  int i = 0;
+  int j = s.size()-1;
+  while(i < j){
+    char temp;
+    temp = s[i];
+    s[i]=s[j];
+    s[j]=temp;
+    i++;
+    j--;
+  }
 
 }
