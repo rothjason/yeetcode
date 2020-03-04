8d701233bf8a895afa02a0b639e9fdce53180a97
Jason <13rothjason@gmail.com>
2020-03-04 10:02:59 -0800

    Added linked-list initializer
    Added for-loop initializer to add nodes to linked list

--- Linked-List/delete-node/delete.cpp
+++ Linked-List/delete-node/delete.cpp
@@ -9,6 +9,8 @@
 
 #include <iostream>
 
+using namespace std;
+
 struct ListNode{
   int val;
   ListNode *next;
@@ -18,7 +20,16 @@ struct ListNode{
 void deleteNode(ListNode *node);
 
 int main(){
+  ListNode test(1);
+  ListNode *ptr = &test;
+  for(int i = 0; i < 10; i++){
+    ptr->next = new ListNode(i);
+    ptr = ptr->next;
+  }
 
+  test.next = new ListNode(2);
+
+  cout << test.val << " " << test.next->val;
 
 
 }
