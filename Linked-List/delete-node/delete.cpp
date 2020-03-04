70bd0bc9b5bf19995ce4f48005a8f74a41edbf9c
Jason <13rothjason@gmail.com>
2020-03-04 09:41:29 -0800

    Initialized delete.cpp
    Created delete-node file and wrote out skeleton code: Linked list def, deleteNode proto+def, main

---
+++ Linked-List/delete-node/delete.cpp
@@ -0,0 +1,30 @@
+/*
+  Objective: Remove a specified node out of a linked list
+
+  Givens:
+  - >1 nodes per list
+  - No duplicates
+  - Target node will not be the tail
+*/
+
+#include <iostream>
+
+struct ListNode{
+  int val;
+  ListNode *next;
+  ListNode(int x): val(x), next(NULL) {}
+};
+
+void deleteNode(ListNode *node);
+
+int main(){
+
+
+
+}
+
+
+void deleteNode(ListNode *node){
+
+
+}
