bd66555f8593178108825d0a8271e983c217c8b7
Jason <13rothjason@gmail.com>
2020-03-07 09:40:00 -0800

    Added algorithm prototype
    Added first iteration (for testing) of best time to buy/sell stock algorithm

--- Arrays/buy-and-sell-stock/stock.cpp
+++ Arrays/buy-and-sell-stock/stock.cpp
@@ -5,8 +5,16 @@ the maximum possible profit
 Givens:
 -Only able to buy on another day until first purchase is sold
 -May buy/sell on as many days as possible
+-May sell then buy on the same day
 
 Algorithm Prototype:
+int profit = 0;
+for(int i = 0; i < v.size(); i++){
+  if(v[i] < v[i+1]){
+    profit += v[i+1]-v[i]
+  }
+}
+return profit
 
 */
 
