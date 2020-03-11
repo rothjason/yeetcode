0c65d29bf2049a2d1aa0855915bb7292799af610
Jason <13rothjason@gmail.com>
2020-03-11 15:51:27 -0700

    Added submission Screenshot

--- Arrays/buy-and-sell-stock/stock.cpp
+++ Arrays/buy-and-sell-stock/stock.cpp
@@ -24,13 +24,33 @@ return profit
 
 using namespace std;
 
-int maxProfite(vector <int> prices);
+int maxProfit(vector <int> prices);
 
 int main(){
+
+  vector <int> v;
+  v.push_back(6);
+  v.push_back(6);
+  v.push_back(3);
+  v.push_back(7);
+  v.push_back(8);
+  v.push_back(8);
+
+  cout << "Max profit: " << maxProfit(v) << endl;
+
+  for (int i=0; i<v.size(); i++){
+    cout << v[i] << ", ";
+  }
 
 
 }
 
 int maxProfit(vector <int> prices){
-
+  int profit = 0;
+  for(int i=1; i < prices.size(); i++){
+    if(prices[i-1] < prices[i]){
+      profit += prices[i]-prices[i-1];
+    }
+  }
+  return profit;
 }
